c4f9771ceb261a3e29b85cc6657db46772867b99
roblivesinottawa <tech.rob@icloud.com>
2021-03-08 14:40:39 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const PORT = 3000
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+
+    }
+
+
+if (req.url === '/api/courses'){
+
+}
+})
+
+server.listen(PORT)
